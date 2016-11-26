73d127250b94bffde1e4e19717367108f108d0ed
Marius Horga <mhorga@gmail.com>
2016-11-26 23:06:47 +0200

    updated to Swift 3

--- ch06/chapter06/MetalView.swift
+++ ch06/chapter06/MetalView.swift
@@ -1,7 +1,7 @@
 //
 //  MetalView.swift
 //  chapter06
-//
+// 
 //  Created by Marius on 2/10/16.
 //  Copyright © 2016 Marius Horga. All rights reserved.
 //
--- ch07/chapter07/MetalView.swift
+++ ch07/chapter07/MetalView.swift
@@ -1,7 +1,7 @@
 //
 //  MetalView.swift
 //  chapter07
-//
+// 
 //  Created by Marius on 2/29/16.
 //  Copyright © 2016 Marius Horga. All rights reserved.
 //
