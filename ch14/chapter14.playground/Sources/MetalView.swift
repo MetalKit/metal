dfc0532882d7767a26666ddac8da22a48b39c1df
Marius Horga <mhorga@gmail.com>
2016-11-29 23:05:57 +0200

    updated to Swift 3

--- ch14/chapter14.playground/Sources/MetalView.swift
+++ ch14/chapter14.playground/Sources/MetalView.swift
@@ -7,15 +7,6 @@ public class MetalView: MTKView, NSWindowDelegate {
     var cps: MTLComputePipelineState! = nil
     var timer: Float = 0
     var timerBuffer: MTLBuffer!
-    var mouseBuffer: MTLBuffer!
-    var pos: NSPoint!
-    
-    override public func mouseDown(_ event: NSEvent) {
-        pos = convertToLayer(convert(event.locationInWindow, from: nil))
-        let scale = layer!.contentsScale
-        pos.x *= scale
-        pos.y *= scale
-    }
     
     required public init(coder: NSCoder) {
         super.init(coder: coder)
@@ -28,12 +19,11 @@ public class MetalView: MTKView, NSWindowDelegate {
     
     override public func draw(_ dirtyRect: NSRect) {
         if let drawable = currentDrawable {
-            let commandBuffer = queue.commandBuffer()
-            let commandEncoder = commandBuffer.computeCommandEncoder()
+            let commandBuffer = queue.makeCommandBuffer()
+            let commandEncoder = commandBuffer.makeComputeCommandEncoder()
             commandEncoder.setComputePipelineState(cps)
             commandEncoder.setTexture(drawable.texture, at: 0)
-            commandEncoder.setBuffer(mouseBuffer, offset: 0, at: 2)
-            commandEncoder.setBuffer(timerBuffer, offset: 0, at: 1)
+            commandEncoder.setBuffer(timerBuffer, offset: 0, at: 0)
             update()
             let threadGroupCount = MTLSizeMake(8, 8, 1)
             let threadGroups = MTLSizeMake(drawable.texture.width / threadGroupCount.width, drawable.texture.height / threadGroupCount.height, 1)
@@ -47,24 +37,21 @@ public class MetalView: MTKView, NSWindowDelegate {
     
     func update() {
         timer += 0.01
-        var bufferPointer = timerBuffer.contents()
-        memcpy(bufferPointer, &timer, sizeof(Float.self))
-        bufferPointer = mouseBuffer.contents()
-        memcpy(bufferPointer, &pos, sizeof(NSPoint.self))
+        let bufferPointer = timerBuffer.contents()
+        memcpy(bufferPointer, &timer, MemoryLayout<Float>.size)
     }
     
     func registerShaders() {
-        queue = device!.newCommandQueue()
-        let path = Bundle.main.pathForResource("Shaders", ofType: "metal")
+        queue = device!.makeCommandQueue()
+        let path = Bundle.main.path(forResource: "Shaders", ofType: "metal")
         do {
             let input = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
-            let library = try device!.newLibrary(withSource: input, options: nil)
-            let kernel = library.newFunction(withName: "compute")!
-            cps = try device!.newComputePipelineState(with: kernel)
+            let library = try device!.makeLibrary(source: input, options: nil)
+            let kernel = library.makeFunction(name: "compute")!
+            cps = try device!.makeComputePipelineState(function: kernel)
         } catch let e {
             Swift.print("\(e)")
         }
-        timerBuffer = device!.newBuffer(withLength: sizeof(Float.self), options: [])
-        mouseBuffer = device!.newBuffer(withLength: sizeof(NSPoint.self), options: [])
+        timerBuffer = device!.makeBuffer(length: MemoryLayout<Float>.size, options: [])
     }
 }
