2fa2654ec80f87514a6aea5c8c63f9da2d4702bd
Marius Horga <mhorga@gmail.com>
2016-11-29 21:44:40 +0200

    updated to Swift 3

--- ch11/chapter11.playground/Sources/MetalView.swift
+++ ch11/chapter11.playground/Sources/MetalView.swift
@@ -2,44 +2,44 @@
 import MetalKit
 
 public class MetalView: NSObject, MTKViewDelegate {
-    
-    public var device: MTLDevice!
-    var queue: MTLCommandQueue!
-    var cps: MTLComputePipelineState!
-    
-    override public init() {
-        super.init()
-        registerShaders()
+  
+  public var device: MTLDevice!
+  var queue: MTLCommandQueue!
+  var cps: MTLComputePipelineState!
+  
+  override public init() {
+    super.init()
+    registerShaders()
+  }
+  
+  func registerShaders() {
+    device = MTLCreateSystemDefaultDevice()!
+    queue = device.makeCommandQueue()
+    let path = Bundle.main.path(forResource: "Shaders", ofType: "metal")
+    do {
+      let input = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+      let library = try device.makeLibrary(source: input, options: nil)
+      let kernel = library.makeFunction(name: "compute")!
+      cps = try device.makeComputePipelineState(function: kernel)
+    } catch let e {
+      Swift.print("\(e)")
     }
-    
-    func registerShaders() {
-        device = MTLCreateSystemDefaultDevice()!
-        queue = device.newCommandQueue()
-        let path = Bundle.main.pathForResource("Shaders", ofType: "metal")
-        do {
-            let input = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
-            let library = try device.newLibrary(withSource: input, options: nil)
-            let kernel = library.newFunction(withName: "compute")!
-            cps = try device.newComputePipelineState(with: kernel)
-        } catch let e {
-            Swift.print("\(e)")
-        }
+  }
+  
+  public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
+  
+  public func draw(in view: MTKView) {
+    if let drawable = view.currentDrawable {
+      let commandBuffer = queue.makeCommandBuffer()
+      let commandEncoder = commandBuffer.makeComputeCommandEncoder()
+      commandEncoder.setComputePipelineState(cps)
+      commandEncoder.setTexture(drawable.texture, at: 0)
+      let threadGroupCount = MTLSizeMake(8, 8, 1)
+      let threadGroups = MTLSizeMake(drawable.texture.width / threadGroupCount.width, drawable.texture.height / threadGroupCount.height, 1)
+      commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
+      commandEncoder.endEncoding()
+      commandBuffer.present(drawable)
+      commandBuffer.commit()
     }
-    
-    public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
-    
-    public func draw(in view: MTKView) {
-        if let drawable = view.currentDrawable {
-            let commandBuffer = queue.commandBuffer()
-            let commandEncoder = commandBuffer.computeCommandEncoder()
-            commandEncoder.setComputePipelineState(cps)
-            commandEncoder.setTexture(drawable.texture, at: 0)
-            let threadGroupCount = MTLSizeMake(8, 8, 1)
-            let threadGroups = MTLSizeMake(drawable.texture.width / threadGroupCount.width, drawable.texture.height / threadGroupCount.height, 1)
-            commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
-            commandEncoder.endEncoding()
-            commandBuffer.present(drawable)
-            commandBuffer.commit()
-        }
-    }
+  }
 }
